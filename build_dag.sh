4ee269bdb18aa4af859e8bc2e1468b80e8a86b4c
Maciej Bak <wsciekly.maciek@gmail.com>
2021-08-20 19:46:49 +0200

    ci: test DAG run

--- build_dag.sh
+++ build_dag.sh
@@ -2,7 +2,11 @@
 snakemake \
     --configfile $1 \
     --dag -np | dot -Tsvg > images/dag.svg
+EXIT_STATUS_1=$?
 
 snakemake \
     --configfile $1 \
     --dag -np | dot -Tpng > images/dag.png
+EXIT_STATUS_2=$?
+
+EXIT EXIT_STATUS_1 || EXIT_STATUS_2
