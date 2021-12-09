2b88016373a3d94002b577b7cb10ce3675efb936
dominikburri <dominik.burri19@gmail.com>
2021-12-09 10:52:51 +0100

    include test data

--- exe_snakemake_local.sh
+++ exe_snakemake_local.sh
@@ -4,7 +4,6 @@ export TMPDIR=~/tmpdir
 snakemake --rerun-incomplete \
     --configfile $1 \
     --use-conda \
-    --conda-prefix $HOME/envs_snakemake \
     --default-resources 'mem_mb=2048' \
-    --resources 'mem_mb=30720' \
-    --cores 10
+    --resources 'mem_mb=8192' \
+    --cores 4
